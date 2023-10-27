dec082d785ea102beb82cd62214d44deaf1ab233
Er. Amit Kumar <121847237+amit7974@users.noreply.github.com>
2023-10-27 19:22:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ClimateApp from './Components/ClimateApp/ClimateApp';
+
+function App() {
+  return (
+    <div className="App">
+
+<ClimateApp />
+
+    </div>
+  
+  );
+}
+
+export default App;
